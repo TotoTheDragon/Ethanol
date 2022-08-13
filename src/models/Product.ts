98c91b2d3ff32f6dd4ae8d1903639bddd8100094
MrDragonPlays <thomasdvandeweerd@gmail.com>
2022-08-14 00:39:07 +0200

    Added price to product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -11,6 +11,8 @@ export default class Product {
 
     private category: string | undefined;
 
+    private price: number | undefined;
+
     private images: EthanolImage[] | undefined;
 
     private brand: string | undefined;
